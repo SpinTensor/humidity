d041fc3909a612165b4a0c823f100b5f41a9735b
Felix Uhl <felix.uhl@ruhr-uni-bochum.de>
2025-08-06 17:25:33 +0200

    ported from rust to zig

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,41 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
+
+fn vapor_pressure(rel_humid: f64, t_kelvin: f64) f64 {
+    const crit_p_h2o: f64 = 22.064 * 1.0e6;
+    const crit_t_h2o: f64 = 647.096;
+    const a1: f64 = -7.85951783;
+    const a2: f64 = 1.84408259;
+    const a3: f64 = -11.7866497;
+    const a4: f64 = 22.6807411;
+    const a5: f64 = -15.9618719;
+    const a6: f64 = 1.80122502;
+    const tau: f64 = 1.0 - t_kelvin / crit_t_h2o;
+    const sat_vp = crit_p_h2o * @exp(crit_t_h2o / t_kelvin * (a1 * std.math.pow(f64, tau, 1.0) +
+        a2 * std.math.pow(f64, tau, 1.5) +
+        a3 * std.math.pow(f64, tau, 3.0) +
+        a4 * std.math.pow(f64, tau, 3.5) +
+        a5 * std.math.pow(f64, tau, 4.0) +
+        a6 * std.math.pow(f64, tau, 7.5)));
+    return sat_vp * rel_humid / 100.0;
+}
+
+fn saturation(t_kelvin: f64) f64 {
+    const vp = vapor_pressure(100.0, t_kelvin);
+    const water_vapor_gas_const = 461.5;
+    return vp / (t_kelvin * water_vapor_gas_const);
+}
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    var t: f64 = -10.0;
+    while (t <= 40.0) : (t += 0.1) {
+        try stdout.print("{d:6.2}", .{t});
+        const sat = saturation(t + 273.15) * 1000.0;
+        var percentage: f64 = 10.0;
+        while (percentage <= 100.0) : (percentage += 10.0) {
+            try stdout.print(" {d:10.6}", .{sat * percentage / 100.0});
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+        try stdout.print("\n", .{});
+    }
 }
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("humidity_lib");
